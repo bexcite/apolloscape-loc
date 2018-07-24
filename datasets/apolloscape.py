32b092ab7df4fd6079f2222d228384d146f8e1f2
Pavlo Bashmakov <pavel.bashmakov@gmail.com>
2018-07-23 18:24:37 -0700

    Initial notepad working again, fixed for new poses

--- datasets/apolloscape.py
+++ datasets/apolloscape.py
@@ -46,7 +46,7 @@ def read_poses_dict_6(fname):
 
 def read_poses_for_camera(record_path, camera_name):
     """Finds and reads poses file for camera_name."""
-    
+
     # Resolve pose.txt file path for camera
     poses_path = os.path.join(record_path, camera_name, 'pose.txt')
     if os.path.exists(poses_path):
@@ -57,15 +57,15 @@ def read_poses_for_camera(record_path, camera_name):
         poses = read_poses_dict_6(poses_path);
     return poses
 
-    
-    
+
+
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
         img = Image.open(f)
         return img.convert('RGB')
-    
-    
+
+
 def check_stereo_paths_consistency(c1, c2):
     """Check that left and right camera images has the same file name timestamp parts. """
     # TODO: May be differencesin 1-2 time units can be neglected?
@@ -77,7 +77,7 @@ def check_stereo_paths_consistency(c1, c2):
         warnings.warn("Not consistent stereo pair paths: \n{}\n{}".format(im1_part,
                                     im2_part))
 
-        
+
 def process_poses(all_poses, pose_format='full-mat',
                   normalize_poses=True):
     # Default pose format is full-mat
@@ -102,10 +102,10 @@ def process_poses(all_poses, pose_format='full-mat',
 
     poses_mean = np.mean(all_poses, axis=0)
     poses_std = np.std(all_poses, axis=0)
-                             
+
     if normalize_poses:
         print('Poses Normalized!')
-        if pose_format == 'quat':            
+        if pose_format == 'quat':
             all_poses[:, :3] -= poses_mean[:3]
             all_poses[:, :3] = np.divide(all_poses[:, :3], poses_std[:3], where=poses_std[:3]!=0)
         else: # 'full-mat'
@@ -115,10 +115,10 @@ def process_poses(all_poses, pose_format='full-mat',
     return all_poses, poses_mean, poses_std
 
 
-    
+
 class Apolloscape(Dataset):
     """Baidu Apolloscape dataset"""
-    
+
     def __init__(self, root, road="road03_seg", transform=None, record=None,
                  normalize_poses=False, pose_format='full-mat'):
         """
@@ -133,48 +133,48 @@ class Apolloscape(Dataset):
         self.road = road
         self.transform = transform
         self.road_path = os.path.join(self.root, self.road)
-        
+
         self.normalize_poses = normalize_poses
         self.pose_format = pose_format
         self.apollo_original_order = True
-        
+
         # Resolve image dir
         image_dir = os.path.join(self.road_path, "ColorImage")
         if not os.path.isdir(image_dir):
             # Sample type dataset (aka zpark-sample)
             image_dir = os.path.join(self.road_path, "image")
-            
+
             # Reset flag, we will use it for Camera orders and images
             self.apollo_original_order = False
         if not os.path.isdir(image_dir):
             warnings.warn("Image directory can't be find in dataset path '{}'. " +
                           "Should be either 'ColorImage' or 'image'".format(self.road_path))
 
-                        
+
         # Resolve pose_dir
         pose_dir = os.path.join(self.road_path, "Pose")
         if not os.path.isdir(pose_dir):
             # Sample type dataset (aka zpark-sample)
             pose_dir = os.path.join(self.road_path, "pose")
-            
+
             # Reset flag, we will use it for Camera orders and images
             self.apollo_original_order = False
         if not os.path.isdir(pose_dir):
             warnings.warn("Pose directory can't be find in dataset path '{}'. " +
                           "Should be either 'Pose' or 'pose'".format(self.road_path))
-        
+
         self.records_list = [f for f in os.listdir(image_dir) if f not in [".DS_Store"]]
         self.records_list = sorted(self.records_list)
-        
-        
+
+
         if not len(self.records_list):
             warnings.warn("Empty records list in provided dataset '{}' for road '{}'".format(self.root, self.road))
-            
+
         # NOTE: In zpark-sample cameras order is in right-left order,
         # which is an opposite of sorted as in original apollo datasets
         self.cameras_list = sorted(os.listdir(os.path.join(image_dir, self.records_list[0])),
                                    reverse=not self.apollo_original_order)
-        
+
         # iterate over all records and store it in internal data
         self.data = []
         for i, r in enumerate(self.records_list):
@@ -182,25 +182,25 @@ class Apolloscape(Dataset):
                            reverse=not self.apollo_original_order)
             cam2s = sorted(glob.glob(os.path.join(image_dir, r, self.cameras_list[1], '*.jpg')),
                            reverse=not self.apollo_original_order)
-            
+
             # Read poses for first camera
             pose1s = read_poses_for_camera(os.path.join(pose_dir, r), self.cameras_list[0])
-                        
+
             # Read poses for second camera
             pose2s = read_poses_for_camera(os.path.join(pose_dir, r), self.cameras_list[1])
-            
+
             c1_idx = 0
             c2_idx = 0
             while c1_idx < len(cam1s) and c2_idx < len(cam2s):
                 c1 = cam1s[c1_idx]
                 c2 = cam2s[c2_idx]
-                
+
                 # Check stereo image path consistency
                 im1 = os.path.basename(c1).split('_')
                 im2 = os.path.basename(c2).split('_')
                 im1_part = '_'.join(im1[:2])
                 im2_part = '_'.join(im2[:2])
-                
+
                 if im1_part != im2_part:
                     # Non-consistent images, drop with the lowest time unit
                     # and repeat with the next idx
@@ -217,23 +217,23 @@ class Apolloscape(Dataset):
                     item.append(pose2s[os.path.basename(c2)])
                     item.append(r)
                     self.data.append(item)
-            
+
                     # Continue with the next pair of images
                     c1_idx += 1
                     c2_idx += 1
 
-                
+
         # Save for extracting poses directly
         self.data_array = np.array(self.data, dtype=object)
-                
+
         # Used as a filter in __len__ and __getitem__
         self.record = record
-        
+
         # Calc mean and std
         all_poses = np.empty((0, 4, 4))
         for p in np.concatenate((self._data_array[:,1], self._data_array[:,3])):
             all_poses = np.vstack((all_poses, np.expand_dims(p, axis=0)))
-        
+
 #         print('poses_poses = {}'.format(self.poses_mean))
 #         print('poses_std = {}'.format(self.poses_std))
 
@@ -256,12 +256,12 @@ class Apolloscape(Dataset):
 #             l = len(all_poses)//2
 #             self._data_array[:,1] = [x for x in all_poses[:l]]
 #             self._data_array[:,3] = [x for x in all_poses[l:]]
-            
+
 #         print('pose sample = {}'.format(self._data_array[0, 1]))
 #         print('poses_mean = {}'.format(self.poses_mean))
 #         print('poses_std = {}'.format(self.poses_std))
 
-        
+
 #         print('all_poses.len = {}'.format(len(all_poses)))
 #         print('poses_poses = {}'.format(self.poses_mean))
 #         print('poses_std = {}'.format(self.poses_std))
@@ -271,21 +271,21 @@ class Apolloscape(Dataset):
         if hasattr(self, 'record_idxs') and self.record_idxs is not None:
             return self._data_array[self.record_idxs]
         return self._data_array
-    
+
     @property
     def all_data_array(self):
         """Returns all data without record filters"""
         return self._data_array
-    
+
     @data_array.setter
     def data_array(self, data_array):
         self._data_array = data_array
 
-        
+
     @property
     def record(self):
         return self._record
-    
+
     @record.setter
     def record(self, record):
         self.record_idxs = None
@@ -293,20 +293,29 @@ class Apolloscape(Dataset):
         if self._record is not None:
             self.record_idxs = self.get_record_idxs(self._record)
 
-            
+
     def poses(self):
         """Get poses list filtered by current record"""
         poses1 = self.data_array[:, 1]
         poses2 = self.data_array[:, 3]
         return poses1, poses2
-    
-    
+
+    def poses_translations(self):
+        """Get translation parts of the poses"""
+        poses1 = self.data_array[:, 1]
+        poses2 = self.data_array[:, 3]
+        if self.pose_format == 'full-mat':
+            poses1 = [p[:3, 3] for p in poses1]
+            poses2 = [p[:3, 3] for p in poses2]
+            return np.array(poses1), np.array(poses2)
+        # TODO: Implement for otherd pose_format - 'quat', etc
+
     def all_poses(self):
         """Get all poses list for all records in dataset"""
         poses1 = self.all_data_array[:, 1]
         poses2 = self.all_data_array[:, 3]
         return poses1, poses2
-            
+
     def get_poses_params(self, all_records=False):
         """Returns min, max, mean and std values the poses"""
         data_array = self.all_data_array if all_records else self.data_array
@@ -315,8 +324,8 @@ class Apolloscape(Dataset):
         all_poses = np.concatenate((poses1, poses2))
         return calc_poses_params(all_poses)
 
-            
-            
+
+
     def get_record_idxs(self, record):
         """Returns idxs array for provided record."""
         if self.data_array is None:
@@ -327,43 +336,43 @@ class Apolloscape(Dataset):
         recs_filter = self.data_array[:, 4] == self.record
         all_idxs = np.arange(len(self.data_array))
         return all_idxs[recs_filter]
-    
-    
+
+
     @property
     def records(self):
         return self.records_list
-    
-    
+
+
     def __len__(self):
         if self.record_idxs is not None:
             return len(self.record_idxs)
         return len(self.data_array)
-    
-    
+
+
     def __getitem__(self, idx):
-        
+
         # If we have a record than work with filtered self.record_idxs
         if self.record_idxs is not None:
             ditem = self._data_array[self.record_idxs[idx]]
         else:
             ditem = self._data_array[idx]
-            
+
         # print("paths = \n{}\n{}".format(ditem[0], ditem[2]));
-        
+
         check_stereo_paths_consistency(ditem[0], ditem[2])
-        
+
         images = []
         poses = []
         for im, pos in zip([0,2], [1,3]):
             img = pil_loader(ditem[im])
             if self.transform is not None:
-                img = self.transform(img)                
-            images.append(img)            
+                img = self.transform(img)
+            images.append(img)
             npos = torch.from_numpy(ditem[pos])
             poses.append(npos.float())
         return images, poses
-    
-    
+
+
     def __repr__(self):
         fmt_str  = "Dataset: " + self.__class__.__name__ + "\n"
         fmt_str += "    Road: {}\n".format(self.road)
--- utils/common.py
+++ utils/common.py
@@ -11,16 +11,16 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True # it's important because not all images r
 def make_video(dataset, record=None, outfile=None):
     if record is not None:
         dataset.record = record
-        
+
     if outfile is None:
         outfile = "./output_data/videos/{}_{}.mp4".format(dataset.road, dataset.record)
 
-        
+
     # Make dirs for video if needed
     video_path = os.path.dirname(outfile)
     if not os.path.exists(video_path):
         os.makedirs(video_path)
-    
+
     FFMpegWriter = manimation.writers['ffmpeg']
     metadata = dict(title='Movie {}, {}'.format(dataset.road, dataset.record),
                     artist='Apolloscape & Pavlo',
@@ -45,7 +45,7 @@ def make_video(dataset, record=None, outfile=None):
 
             # Store video frame
             writer.grab_frame()
-            
+
     # Clear figure
     plt.close(fig)
 
@@ -57,7 +57,7 @@ def draw_record(dataset, record=None, idx=None, restore_record=True, axes=None):
     if record is not None and restore_record:
         saved_rec = dataset.record
         dataset.record = record
-    
+
     fig = None
     if axes is None:
         fig = plt.figure(figsize=(8, 8))
@@ -69,13 +69,13 @@ def draw_record(dataset, record=None, idx=None, restore_record=True, axes=None):
         ax1 = axes[0]
         ax2 = axes[1]
         ax3 = axes[2]
-        
+
         # Clean previous data
         ax1.cla()
         ax2.cla()
         ax3.cla()
 
-        
+
     # Set up axes
     ax1.axis('off')
     ax2.axis('off')
@@ -85,17 +85,17 @@ def draw_record(dataset, record=None, idx=None, restore_record=True, axes=None):
     ax3.view_init(50, 30)
 
 
-    # Sample a data point from the record 
+    # Sample a data point from the record
     if idx is None:
         idx = np.random.randint(len(dataset))
     images, poses = dataset[idx]
-    
+
 
     p_min, p_max, p_mean, p_std = dataset.get_poses_params()
-#     print("min = {}".format(p_min))
-#     print("max = {}".format(p_max))
-#     print("mean = {}".format(p_mean))
-#     print("std = {}".format(p_std))
+    # print("min = {}".format(p_min))
+    # print("max = {}".format(p_max))
+    # print("mean = {}".format(p_mean))
+    # print("std = {}".format(p_std))
 
     ax3.set_title("{} {} ({} of {})".format(dataset.road,
         dataset.record if dataset.record is not None else "",
@@ -107,29 +107,32 @@ def draw_record(dataset, record=None, idx=None, restore_record=True, axes=None):
     ax3.set_zlim(int(p_min[2]), int(p_max[2]) + 1)
 
     # Show all poses for selected record
-    poses1, poses2 = dataset.poses()
+    poses1, poses2 = dataset.poses_translations()
     mid_poses = 0.5 * (poses1 + poses2)
-    
+
+    # print('mid_poses = {}'.format(mid_poses))
+
     draw_poses(ax3, mid_poses, proj=True, proj_z=int(p_min[2]))
-    
+
     # Show current sample pose
-    draw_poses(ax3, [0.5*np.sum(poses, axis=0)], c='r', s=60, proj=True, proj_z=int(p_min[2]))
-    
+    mid_pose = 0.5 * (poses[0][:3, 3] + poses[1][:3, 3])
+    draw_poses(ax3, [mid_pose], c='r', s=60, proj=True, proj_z=int(p_min[2]))
+
     # Show current sample camera images
     ax1.imshow(images[0])
     ax2.imshow(images[1])
-    
-    
+
+
     # Restore record if it was custom
     if record is not None and restore_record:
         dataset.record = saved_rec
-        
+
     return fig
 
 
 def draw_poses(ax, poses, c='b', s=20, proj=False, proj_z=0):
     """Draws the list of poses.
-    
+
     Args:
         ax (Axes3D): 3D axes
         poses (list): Poses list
@@ -140,25 +143,26 @@ def draw_poses(ax, poses, c='b', s=20, proj=False, proj_z=0):
     """
     coords = np.zeros((len(poses), 3))
     for i, p in enumerate(poses):
-        coords[i] = p[:3, 3]
+        # coords[i] = p[:3, 3]
+        coords[i] = p
     ax.scatter(coords[:, 0], coords[:, 1], coords[:, 2], c=c, s=s)
-    
+
     if proj:
         if len(poses) > 1:
             ax.plot(coords[:, 0], coords[:, 1], proj_z, c='g')
         elif len(poses) == 1:
             ax.scatter(coords[:, 0], coords[:, 1], proj_z, c=c)
-    
+
 #     XYZ = np.tile(np.array([0., 0., 0., 1.]).reshape(4, -1), 1)
 #         XYZp = np.matmul(p, XYZ)
 #         ax.scatter(XYZp[0], XYZp[1], XYZp[2], c=c, s=s)
 
-        
+
 def draw_poses_list(ax, poses_list):
     """Draw list of lists of poses. Use to draw several paths."""
     for poses in poses_list:
         draw_poses(ax, poses)
-        
+
 def calc_poses_params(poses):
     """Calculates min, max, mean and std of translations of the poses"""
     p = poses[0]
@@ -171,5 +175,5 @@ def calc_poses_params(poses):
     p_max = np.max(allp, axis=0)
     p_mean = np.mean(allp, axis=0)
     p_std = np.std(allp, axis=0)
-    
+
     return p_min, p_max, p_mean, p_std
